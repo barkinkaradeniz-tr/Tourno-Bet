b9e3765879a84712ace6ebaa3eb440276513c5f0
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-05-02 20:19:24 +0200

    Add a register and login path

---
+++ backend/src/lib/middlewares/local-strategy.ts
@@ -0,0 +1,48 @@
+import { Strategy as LocalStrategy } from "passport-local";
+import * as AccountManager from "../../domains/account/manager";
+import { UserDocument } from "../../domains/account/models/user";
+import Account from "../../domains/account/models/account";
+import { RequestHandler } from "express";
+import { UnauthorizedRequestError, BadRequestError } from "../errors";
+
+export interface LoggedInRequest extends Express.Request {
+  user: UserDocument;
+  body: any;
+  query: any;
+  params: any;
+}
+
+export const ensureOrphands: RequestHandler = (req, res, next) => {
+  if (req.isAuthenticated())
+    return next(
+      new BadRequestError(
+        "There is an ongoing session. Please logout in order to perform this action."
+      )
+    );
+  next();
+};
+
+export const ensureAuthentication: RequestHandler = (req, res, next) => {
+  if (!req.isAuthenticated()) return next(new UnauthorizedRequestError());
+
+  const user = req.user as UserDocument;
+
+  console.log(`Authenticated request from origin ${req.headers.origin}`);
+
+  next();
+};
+
+export default new LocalStrategy(
+  {
+    usernameField: "email",
+    passwordField: "password",
+  },
+  async (email, password, done) => {
+    try {
+      const user = await AccountManager.authenticate(email, password);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  }
+);
--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -1,9 +1,60 @@
 import { AsyncRouter } from "express-async-router";
+import { celebrate, Joi, Segments } from "celebrate";
+import passport from "passport";
+import passwordComplexity from "joi-password-complexity";
+import * as AccountManager from "../domains/account/manager";
+import {
+  ensureAuthentication,
+  LoggedInRequest,
+  ensureOrphands,
+  // ensureEmailVerified,
+} from "../lib/middlewares/local-strategy";
 
 const router = AsyncRouter();
 
-router.post("/login", async (req, res) => {
-  res.json({ status: "ok" });
-});
+router.post(
+  "/session",
+  ensureOrphands,
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string(),
+    }),
+  }),
+  passport.authenticate("local", { failWithError: true }),
+  (req: LoggedInRequest) => {
+    return req.user;
+  }
+);
+
+router.post(
+  "/user",
+  ensureOrphands,
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: passwordComplexity(),
+      firstName: Joi.string().required().min(2).max(128),
+      lastName: Joi.string().required().min(2).max(128),
+    }),
+  }),
+  async (req) => {
+    const { email, password, ...rest } = req.body;
+
+    const guest = await AccountManager.registerUser(
+      {
+        account: { email, password, emailVerified: false },
+        ...rest,
+      },
+      req.query.redirectUrl as string
+    );
+
+    return req.login(guest, (err: any) => {
+      if (err) throw err;
+
+      return guest;
+    });
+  }
+);
 
 export default router;
