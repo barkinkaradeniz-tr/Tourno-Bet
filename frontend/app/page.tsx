0421c1ff8d26f17c8d482db072acbe36b60bd616
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-05-06 16:29:27 +0200

    Initialize the nextjs frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/common/login";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Login />
+    </main>
+  );
+}
