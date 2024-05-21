10983cb538cff210865755f2c22b5029512e6f0d
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-05-21 21:22:17 +0200

    Ensure that logged in users' redirect to home

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,16 @@
 import Login from '@/common/modules/auth/components/login';
+import { getCurrentUser } from '@/common/modules/auth/services/auth-service';
 
-export default function Page() {
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+export default function LoginPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(getCurrentUser());
+  }, []);
+
   return <Login />;
 }
