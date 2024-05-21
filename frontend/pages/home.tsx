2772867c64eac74292cdd5eed336bf2f3888c1f5
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-05-21 21:23:01 +0200

    Add a simple home page

---
+++ frontend/pages/home.tsx
@@ -0,0 +1,10 @@
+import AppLayout from '@/common/components/app-layout';
+
+export default function Home() {
+  return (
+    <AppLayout>
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+    </AppLayout>
+  );
+}
